e7402162499e944157901ad09f7ab4bbff43458b
Bhavishya Bodwani <bhavishya1077@gmail.com>
2026-06-30 15:39:33 +0530

    added login route with bcrypt.compare password verification

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,26 @@ const router= express.Router()
 const bcrypt = require('bcrypt');
 const user= require("../models/user")
 
-router.post('/login',(req,res)=>{
-    console.log(req.body)
+router.post('/login',async (req,res)=>{
+    const email= req.body.email
+    const password= req.body.password
+
+    try{
+        const foundUser=await user.findOne({email})
+        if(!foundUser){
+        res.status(400).json({msg:"Invalid email and password"}) 
+        }else{
+        let isCorrect = await bcrypt.compare(password,foundUser.password)
+        if(!isCorrect){
+            res.status(400).json({msg:"Invalid email and password"})
+        }else{
+            res.status(200).json({msg:"Login Successfully"})
+        }
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).json({msg:"Internal Error"});
+    }
 })
 
 router.post('/signup',async (req,res)=>{
